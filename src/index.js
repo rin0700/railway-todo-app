56463c6283a33b1369a40bae9455f15a6b1fce3a
rin0700 <135789382+rin0700@users.noreply.github.com>
2024-07-15 21:00:18 +0900

    PrettierFormat/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { CookiesProvider } from "react-cookie";
-import { Provider } from "react-redux";
-import { store } from "./store"
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from 'react-cookie';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <CookiesProvider>
       <App />
     </CookiesProvider>
-  </Provider>,
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
